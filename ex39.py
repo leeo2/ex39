2cd93457e913c9ef04dbb861d7ff8fbed94fc7e0
leeo2 <leeo@hartwick.edu>
2019-11-06 21:25:59 -0500

    Edited

--- ex39.py
+++ ex39.py
@@ -1,3 +1,40 @@
 # Olivia Lee
 # 11-6-19
 # Dictionaries
+
+# Create a mapping of state to abbreviation
+
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI',
+}
+
+# Create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# print out some cities
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+# print some states
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+# print every state abbreviation
+print('-' * 10)
+
+
+
